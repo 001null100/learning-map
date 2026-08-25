4e9dbccf58170a1bd5d63694dd643892ff31faf2
001null100 <128686360+001null100@users.noreply.github.com>
2026-08-25 21:21:48 +0300

    Wrap app in appearance provider

--- src/main.jsx
+++ src/main.jsx
@@ -2,10 +2,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import '@xyflow/react/dist/style.css';
 import './styles.css';
+import './v25.css';
 import App from './App.jsx';
+import { AppearanceProvider } from './appearance/AppearanceProvider.jsx';
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <AppearanceProvider>
+      <App />
+    </AppearanceProvider>
   </React.StrictMode>,
 );
